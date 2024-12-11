c73739bd9026a4eb43837ff63f059d4cd37339de
Esmaeil Sheikhi <es.sheikhi@gmail.com>
2024-12-11 12:16:17 +0330

    Adding MenuViewComponent

---
+++ IdentityManager/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+namespace IdentityManager.ViewComponents
+{
+    public class MenuViewComponent:ViewComponent
+    {
+        private readonly IConfiguration _configuration;
+        public MenuViewComponent(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var defaultList=new List<string>
+            {
+                "Home"
+            };
+            List<string> menus = _configuration.GetSection("Menus").Get<List<string>>() ?? defaultList;
+            return View(menus);
+        }
+    }
+}
